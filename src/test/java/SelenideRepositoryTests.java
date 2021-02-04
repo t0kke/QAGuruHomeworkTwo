36af9fcf38dde31ef7003a43d81e741316969715
German <k4tger@gmail.com>
2021-02-04 13:41:30 +0300

    Initial commit

---
+++ src/test/java/SelenideRepositoryTests.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+public class SelenideRepositoryTests 
+{
+    @Test
+    void checkSampleCodeJUnit5OnPage() {
+        open("https://github.com/selenide/selenide");
+        $("span[data-content='Wiki']").click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $x("//a[text()='SoftAssertions']").click();
+        $("div[class='markdown-body']").shouldHave(text("Using JUnit5 extend test class"));
+    }
+}
