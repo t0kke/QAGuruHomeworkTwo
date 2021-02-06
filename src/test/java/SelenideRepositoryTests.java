e58528d7e2d1f36ce67b863b264adee5460b23a2
German <k4tger@gmail.com>
2021-02-06 21:57:37 +0300

    Refactoring selector

--- src/test/java/SelenideRepositoryTests.java
+++ src/test/java/SelenideRepositoryTests.java
@@ -1,16 +1,17 @@
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
-public class SelenideRepositoryTests 
-{
+public class SelenideRepositoryTests {
     @Test
     void checkSampleCodeJUnit5OnPage() {
         open("https://github.com/selenide/selenide");
         $("span[data-content='Wiki']").click();
         $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
-        $x("//a[text()='SoftAssertions']").click();
+        $(byText("SoftAssertions")).click();
         $("div[class='markdown-body']").shouldHave(text("Using JUnit5 extend test class"));
     }
 }
